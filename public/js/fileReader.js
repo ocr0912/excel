586ec0090b7f2384a548514822f655986bcd8846
ocr0912 <ocr0912@gmail.com>
2021-02-26 01:31:32 +0900

    코드 순서 정리

--- public/js/fileReader.js
+++ public/js/fileReader.js
@@ -4,15 +4,18 @@ window.onload = () => {
     // 파일을 불러오면, 정보를 읽는다.
     fileReader.addEventListener("change", () => {
         const file = fileReader.files[0];
-        const reader = new FileReader();
 
-        // 파일 불러오기 시작.
         if (file) {
-            reader.onload = () => console.log(reader.result);
-            fileReader.value = "";
-            reader.onerror = err => console.error(err);
+            const reader = new FileReader();
 
+            reader.onload = () => console.log(reader.result);
+            reader.onerror = err => console.error(err);
+            
+            // 파일 불러오기 시작.
             reader.readAsText(file);
+
+            // 값을 초기화.
+            fileReader.value = "";
         }
     });
 };
